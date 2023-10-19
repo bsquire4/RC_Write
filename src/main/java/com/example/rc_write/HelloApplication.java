a2fdac6643b3a98bf677fd88765fd3fec673ad6c
bsquire4 <bensquirep4@gmail.com>
2023-10-19 17:30:36 +0100

    input validation and relative file paths

--- src/main/java/com/example/rc_write/HelloApplication.java
+++ src/main/java/com/example/rc_write/HelloApplication.java
@@ -31,19 +31,6 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
-//        stage.setTitle("JavaFX App");
-//        BorderPane border = new BorderPane();
-//
-//
-//        Label label = new Label("Add a Map");
-//        label.prefWidth(1000);
-//
-//        border.setTop(label);
-////        border.setLeft(addVBox());
-//
-//        border.setCenter(addGridPane(stage));
-//
-////        border.setRight(addFlowPane());
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         double width = screenSize.getWidth() *0.8;
         double height = screenSize.getHeight() *0.8;
@@ -65,24 +52,5 @@ public class HelloApplication extends Application {
         launch();
     }
 
-    public GridPane addGridPane(Stage stage) {
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(0, 10, 0, 10));
-
-        FileChooser fileChooser = new FileChooser();
-
-        Button button = new Button("Select File");
-        button.setOnAction(e -> {
-            File selectedFile = fileChooser.showOpenDialog(stage);
-        });
-        button.setPrefSize(100,100);
-        grid.add(button,1,1);
-
-
-
-        return grid;
-    }
 
 }
--- src/main/java/com/example/rc_write/HelloController.java
+++ src/main/java/com/example/rc_write/HelloController.java
@@ -2,15 +2,11 @@ package com.example.rc_write;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,64 +35,39 @@ public class HelloController {
 
     public File imageFile;
 
-
-
-
-    public static void Starting(Stage stage)
-    {
-        FileChooser fileChooser = new FileChooser();
-
-        Button button = new Button("Select File");
-        button.setOnAction(e -> {
-            File selectedFile = fileChooser.showOpenDialog(stage);
-        });
-
-    }
-    @FXML
-    protected void onHelloButtonClick() throws FileNotFoundException {
-        welcomeText.setText("Welcome to JavaFX Application!");
-
-    }
-
     public void onFileSelect() {
         FileChooser fileChooser = new FileChooser();
         Stage stage = new Stage();
         imageFile = fileChooser.showOpenDialog(stage);
-        try{
+        try {
             InputStream stream = new FileInputStream(imageFile);
             Image image = new Image(stream);
             ImageBox.setImage(image);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("FILE NOT ACCEPTED");
             System.out.println(e);
         }
     }
-    public void change(ActionEvent event)
-    {
-        if(ChoiceBox1.isSelected())
-        {
+
+    public void change(ActionEvent event) {
+        //Making sure only 1 choice can be "Best"
+        if (ChoiceBox1.isSelected()) {
             ChoiceBox2.setDisable(true);
             ChoiceBox3.setDisable(true);
             ChoiceBox4.setDisable(true);
-        } else if (ChoiceBox2.isSelected())
-        {
+        } else if (ChoiceBox2.isSelected()) {
             ChoiceBox1.setDisable(true);
             ChoiceBox3.setDisable(true);
             ChoiceBox4.setDisable(true);
-        } else if (ChoiceBox3.isSelected())
-        {
+        } else if (ChoiceBox3.isSelected()) {
             ChoiceBox1.setDisable(true);
             ChoiceBox2.setDisable(true);
             ChoiceBox4.setDisable(true);
-        } else if (ChoiceBox4.isSelected())
-        {
+        } else if (ChoiceBox4.isSelected()) {
             ChoiceBox1.setDisable(true);
             ChoiceBox2.setDisable(true);
             ChoiceBox3.setDisable(true);
-        }else
-        {
+        } else {
             ChoiceBox1.setDisable(false);
             ChoiceBox2.setDisable(false);
             ChoiceBox3.setDisable(false);
@@ -105,127 +76,179 @@ public class HelloController {
     }
 
     public void onFinishButtonPress(ActionEvent event) {
-        String filelocation = "C:/Users/Ben04/IdeaProjects/RC/src/main/resources/MapsFile.csv";
-        File file = new File(filelocation);
-
+        String MapFilesLocation = "../MapsFile.csv";
+        String counterFileLocation = "../Counter.csv";
         int id = 0;
         String imageLocation = String.valueOf(imageFile);
         int choice = 0;
         int correctChoice = 0;
         List<String> colours = new ArrayList<>();
         List<String> distances = new ArrayList<>();
-        if(!(TextColour1.getText().equals("") || TextDistance1.equals("")) )
-        {
-            choice++;
-            colours.add(TextColour1.getText());
-            distances.add(TextDistance1.getText());
-            if(ChoiceBox1.isSelected())
-            {
-                correctChoice = 1;
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        boolean validInput = true;
+        if (!(TextColour1.getText().equals("") || TextDistance1.getText().equals(""))) {
+            if (isNumber(TextColour1.getText()) || !isNumber(TextDistance1.getText())) {
+                validInput = false;
+                alert.setTitle("Invalid input");
+                alert.setHeaderText("Invalid Input");
+                alert.setContentText("You have entered an invalid input for the first colour or distance box");
+                alert.showAndWait();
+            } else {
+                choice++;
+                colours.add(TextColour1.getText());
+                distances.add(TextDistance1.getText());
+                if (ChoiceBox1.isSelected()) {
+                    correctChoice = 1;
+                }
             }
         }
-        if(!(TextColour2.getText().equals("") || TextDistance2.equals("")) )
-        {
-            choice++;
-            colours.add(TextColour2.getText());
-            distances.add(TextDistance2.getText());
-            if(ChoiceBox2.isSelected())
-            {
-                correctChoice = 2;
+        if (!(TextColour2.getText().equals("") || TextDistance2.getText().equals(""))) {
+            if (isNumber(TextColour2.getText()) || !isNumber(TextDistance2.getText())) {
+                validInput = false;
+                alert.setTitle("Invalid input");
+                alert.setHeaderText("Invalid Input");
+                alert.setContentText("You have entered an invalid input for the second colour or distance box");
+                alert.showAndWait();
+            } else {
+                choice++;
+                colours.add(TextColour2.getText());
+                distances.add(TextDistance2.getText());
+                if (ChoiceBox2.isSelected()) {
+                    correctChoice = 2;
+                }
             }
         }
-        if(!(TextColour3.getText().equals("") || TextDistance3.equals("") ))
-        {
-            choice++;
-            colours.add(TextColour3.getText());
-            distances.add(TextDistance3.getText());
-            if(ChoiceBox3.isSelected())
-            {
-                correctChoice = 3;
+        if (!(TextColour3.getText().equals("") || TextDistance3.getText().equals(""))) {
+            if (isNumber(TextColour3.getText()) || !isNumber(TextDistance3.getText())) {
+                validInput = false;
+                alert.setTitle("Invalid input");
+                alert.setHeaderText("Invalid Input");
+                alert.setContentText("You have entered an invalid input for the third colour or distance box");
+                alert.showAndWait();
+            } else {
+                choice++;
+                colours.add(TextColour3.getText());
+                distances.add(TextDistance3.getText());
+                if (ChoiceBox3.isSelected()) {
+                    correctChoice = 3;
+                }
             }
         }
-        if(!(TextColour4.getText().equals("") || TextDistance4.equals("")) )
-        {
-            choice++;
-            colours.add(TextColour4.getText());
-            distances.add(TextDistance4.getText());
-            if(ChoiceBox4.isSelected())
-            {
-                correctChoice = 4;
+        if (!(TextColour4.getText().equals("") || TextDistance4.getText().equals(""))) {
+            if (isNumber(TextColour4.getText()) || !isNumber(TextDistance4.getText())) {
+                validInput = false;
+                alert.setTitle("Invalid input");
+                alert.setHeaderText("Invalid Input");
+                alert.setContentText("You have entered an invalid input for the forth colour or distance box");
+                alert.showAndWait();
+            } else {
+                choice++;
+                colours.add(TextColour4.getText());
+                distances.add(TextDistance4.getText());
+                if (ChoiceBox4.isSelected()) {
+                    correctChoice = 4;
+                }
             }
         }
 
-        String[] coloursString = new String[choice];
+        if ((choice < 2) && validInput) {
+            validInput = false;
+            alert.setTitle("Invalid Input");
+            alert.setHeaderText("Invalid Input");
+            alert.setContentText("There needs to be more than 1 route choice !");
+            alert.showAndWait();
+        }
 
+        if (imageLocation.equals("") && validInput) {
+            validInput = false;
+            alert.setTitle("Invalid Input");
+            alert.setHeaderText("Invalid Input");
+            alert.setContentText("Image File needed");
+            alert.showAndWait();
+        }
+        if (correctChoice == 0 && validInput) {
+            validInput = false;
+            alert.setTitle("Invalid Input");
+            alert.setHeaderText("Invalid Input");
+            alert.setContentText("Which one is the Best choice?");
+            alert.showAndWait();
+        }
 
-        String[] distancesString = new String[choice];
+        if (validInput) {
+            String[] coloursString = new String[choice];
+            String[] distancesString = new String[choice];
+            try {
+                Scanner counterScanner = new Scanner(new File(counterFileLocation));
+                counterScanner.useDelimiter(",");
+                id = Integer.parseInt(counterScanner.next());
 
-        try{
-            Scanner sc = new Scanner(new File("C:/Users/Ben04/IdeaProjects/RC/src/main/resources/Counter.csv"));
-            sc.useDelimiter(",");
-            id = Integer.parseInt(sc.next());
-            System.out.println(id);
-            id++;
+                System.out.println(id);
+                id++;
+                while (counterScanner.hasNext()) {
+                    List<String> tempList = new ArrayList<>();
+                    tempList.add(counterScanner.next());
+                }
 
-            while (sc.hasNext())
-            {
-                List<String> tempList = new ArrayList<>();
-                tempList.add(sc.next());
+                counterScanner.close();
+            } catch (Exception e) {
+                System.out.println("ERROR READING COUNTER FILE");
+                System.out.println(e);
             }
 
-            sc.close();
-        }catch (Exception e)
-        {
-            System.out.println("ERROR READING FILE");
-            System.out.println(e);
-        }
-
-        List<String[]> dataLines = new ArrayList<>();
-        String[] fileinput = new String[4 + choice + choice];
-        fileinput[0] = Integer.toString(id);
-        fileinput[1] = imageLocation;
-        fileinput[2] = Integer.toString(choice);
-        fileinput[3] = Integer.toString(correctChoice);
-        int counter = 0;
-        for (String i:colours) {
-            fileinput[4 + counter] = i;
-            counter++;
-        }
-        counter = 0;
-        for (String x:distances)
-        {
-            fileinput[4 + choice+ counter] = x;
-            counter++;
-        }
-
-        try{
-           FileWriter fileWriter1 = new FileWriter("C:/Users/Ben04/IdeaProjects/RC/src/main/resources/Counter.csv", false);
-            System.out.println("ID:" + id);
-           fileWriter1.write(String.valueOf(id));
-           fileWriter1.write(",");
-           fileWriter1.flush();
-           fileWriter1.close();
-
-            FileWriter fileWriter = new FileWriter(filelocation,true);
-            for (String y:fileinput) {
-                System.out.println(y);
-                fileWriter.write(y);
-                fileWriter.flush();
-                fileWriter.write(",");
-                fileWriter.flush();
+            List<String[]> dataLines = new ArrayList<>();
+            String[] fileinput = new String[4 + choice + choice];
+            fileinput[0] = Integer.toString(id);
+            fileinput[1] = imageLocation;
+            fileinput[2] = Integer.toString(choice);
+            fileinput[3] = Integer.toString(correctChoice);
+            int counter = 0;
+            for (String i : colours) {
+                fileinput[4 + counter] = i;
+                counter++;
             }
-        }
-        catch (Exception e)
-        {
-            System.out.println("FAILED TO WRITE TO FILE");
-            System.out.println(e);
+            counter = 0;
+            for (String x : distances) {
+                fileinput[4 + choice + counter] = x;
+                counter++;
+            }
+
+            try {
+                FileWriter fileWriter = new FileWriter(MapFilesLocation, true);
+                for (String y : fileinput) {
+                    System.out.println(y);
+                    fileWriter.write(y);
+                    fileWriter.flush();
+                    fileWriter.write(",");
+                    fileWriter.flush();
+                }
+
+            } catch (Exception e) {
+                System.out.println("FAILED TO WRITE TO MAPFILESLOCATION FILE");
+                System.out.println(e);
+            }
+
+            try {
+                FileWriter fileWriter1 = new FileWriter(counterFileLocation, false);
+                System.out.println("ID:" + id);
+                fileWriter1.write(String.valueOf(id));
+                fileWriter1.write(",");
+                fileWriter1.flush();
+                fileWriter1.close();
+            } catch (Exception e) {
+                System.out.println("FAILED TO WRITE TO COUNTER FILE");
+                System.out.println(e);
+
+            }
+
         }
     }
+
     public String convertToCSV(String[] data) {
         return Stream.of(data)
                 .map(this::escapeSpecialCharacters)
                 .collect(Collectors.joining(","));
     }
+
     public String escapeSpecialCharacters(String data) {
         String escapedData = data.replaceAll("\\R", " ");
         if (data.contains(",") || data.contains("\"") || data.contains("'")) {
@@ -233,5 +256,13 @@ public class HelloController {
             escapedData = "\"" + data + "\"";
         }
         return escapedData;
+    }
+
+    static boolean isNumber(String s) {
+        for (int i = 0; i < s.length(); i++)
+            if (Character.isDigit(s.charAt(i)) == false)
+                return false;
+
+        return true;
     }
 }
